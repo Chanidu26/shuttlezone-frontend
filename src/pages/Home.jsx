da3788447c99eb2d2616047fb082877569c452a0
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-10-11 10:56:14 +0530

    About section initialized

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,11 @@ import court2 from '../assets/images/court2.jpg'
 import court4 from '../assets/images/court4.jpg'
 import player from '../assets/images/player.png'
 import icon1 from '../assets/images/icon01.png'
+import icon2 from '../assets/images/icon02.png'
+import icon3 from '../assets/images/icon03.png'
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
+import About from '../components/About/About'
 const Home = () => {
   return (
     <>
@@ -91,8 +94,44 @@ const Home = () => {
                 Find a court near you.
               </p>
 
-              <Link to = '/courts' className='w-[44px] h-[44px] rounded-full border border-solid border-
-              [#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+              <Link to = '/courts' className='w-[44px] h-[44px] rounded-full border border-solid border-black
+               mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                < BsArrowRight className='group-hover:text-white w-6 h-5 '/>
+              </Link>
+            </div>
+           </div>
+
+           <div className='py-[30px] px-5'>
+            <div className='flex items-center justify-center'>
+                <img src={icon2}></img>
+            </div>
+
+            <div className='mt-[30px]'>
+              <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Location</h2>
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+                Find a court near you.
+              </p>
+
+              <Link to = '/courts' className='w-[44px] h-[44px] rounded-full border border-solid border-black 
+              mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                < BsArrowRight className='group-hover:text-white w-6 h-5 '/>
+              </Link>
+            </div>
+           </div>
+
+           <div className='py-[30px] px-5'>
+            <div className='flex items-center justify-center'>
+                <img src={icon3}></img>
+            </div>
+
+            <div className='mt-[30px]'>
+              <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Book a Court</h2>
+              <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+                Find a court near you.
+              </p>
+
+              <Link to = '/courts' className='w-[44px] h-[44px] rounded-full border border-solid border-black 
+              mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
                 < BsArrowRight className='group-hover:text-white w-6 h-5 '/>
               </Link>
             </div>
@@ -102,6 +141,9 @@ const Home = () => {
 
       </div>
     </section>
+
+    {/*about section*/}
+    <About/>
     </>
   )
 }
