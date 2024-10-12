eee292ffd5e930bb8449a0a38e19b90c64bc5ce5
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-10-12 22:34:23 +0530

    update bg-color of footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
   ]
   const year = new Date().getFullYear()
   return (
-    <footer className='pb-16 pt-10'>
+    <footer className='pb-16 pt-10 bg-slate-100'>
       <div className='container'>
         <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
           <div>
