c2c861a09e3f362728eff6824a4ce6dba32c54a8
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-10-15 12:53:28 +0530

    contact section added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      <h2 className='heading text-center pt-4'>Contact</h2>
-    </div>
+    <section className='pt-5'>
+      <div className='px-4 mx-auto max-w-screen-md '>
+        <h2 className='heading text-center'>Contact us</h2>
+        <p className='text_para text-center'>
+          Do you have technical issue? Do you want to send a Feedback about us? Let us know
+        </p>
+        <form action='#' className='space-y-8 pt-4'>
+          <div>
+            <label htmlFor='email' className='form_label'>
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              className='form_input mt-1'
+              placeholder='Enter your email'
+            />
+          </div>
+
+          <div>
+            <label htmlFor='email' className='form_label'>
+              Message
+            </label>
+            <input
+              type='email'
+              id='email'
+              className='form_input mt-1'
+              placeholder='Enter your email'
+            />
+          </div>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
